e93a2fa207ed2eeea14ebec8fed638b9e9a19c4f
Micah Nicole Chu Im <micah_chuim@yahoo.com>
2016-11-29 21:36:28 +0800

    Update Launcher.java

--- src/packageRPG/Launcher.java
+++ src/packageRPG/Launcher.java
@@ -1,7 +1,7 @@
 package packageRPG;
 
 public class Launcher {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Game game = new Game("Westerrose", 640, 360);
 		game.start();
 	}
