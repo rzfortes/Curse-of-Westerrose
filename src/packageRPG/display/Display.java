f74c5ac4c858b45220d588b74e996b879d5f2d9a
Micah Nicole Chu Im <micah_chuim@yahoo.com>
2016-11-29 21:46:51 +0800

    Add files via upload

--- src/packageRPG/display/Display.java
+++ src/packageRPG/display/Display.java
@@ -5,52 +5,49 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
-
 public class Display {
-	
+
 	private JFrame frame;
-	private Canvas canvas; //allows to draw graphics on it
-	
+	private Canvas canvas; // allows to draw graphics on it
+
 	private String title;
 	private int width, height;
-	
-	public Display(String title, int width, int height){
+
+	public Display(String title, int width, int height) {
 		this.title = title;
 		this.width = width;
 		this.height = height;
-		
+
 		createDisplay();
 	}
-	
-	private void createDisplay(){
+
+	private void createDisplay() {
 		frame = new JFrame(title);
 		frame.setSize(width, height);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false); //user cannot resize the window
-		frame.setLocationRelativeTo(null); //window appears in the center of the screen
-		frame.setVisible(true); //set it visible so we could see it
-		
+		frame.setResizable(false); // user cannot resize the window
+		frame.setLocationRelativeTo(null); // window appears in the center of
+											// the screen
+		frame.setVisible(true); // set it visible so we could see it
+
 		canvas = new Canvas();
 		canvas.setPreferredSize(new Dimension(width, height));
 		canvas.setMaximumSize(new Dimension(width, height));
 		canvas.setMinimumSize(new Dimension(width, height));
 		canvas.setFocusable(false);
-		
-		
+
 		frame.add(canvas);
 		frame.pack();
-		
+
 	}
-	
-	public Canvas getCanvas(){
+
+	public Canvas getCanvas() {
 		return canvas;
-		
+
 	}
-	
-	public JFrame getFrame(){ //returns the frame in the display class
+
+	public JFrame getFrame() { // returns the frame in the display class
 		return frame;
 	}
-	
-	
-	
+
 }
