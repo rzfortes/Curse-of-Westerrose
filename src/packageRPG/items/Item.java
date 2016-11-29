3467c4f0541b1e62f08e1bf685d651d1e64c9089
Micah Nicole Chu Im <micah_chuim@yahoo.com>
2016-11-29 21:45:16 +0800

    Add files via upload

--- src/packageRPG/items/Item.java
+++ src/packageRPG/items/Item.java
@@ -8,70 +8,69 @@ import packageRPG.Handler;
 import packageRPG.gfx.Assets;
 
 public class Item {
-	
+
 	// item handler
-	
+
 	public static Item[] items = new Item[256];
 	public static Item woodITem = new Item(Assets.wood, "Wood", 0);
 	public static Item rockITem = new Item(Assets.rock, "Rock", 1);
-	
+	public static Item mushroom = new Item(Assets.mushroom, "Mushroom", 2);
+
 	// class
-	
+
 	public static final int ITEMWIDTH = 32, ITEMHEIGHT = 32;
 	protected Handler handler;
 	protected BufferedImage texture;
 	protected String name;
 	protected final int id;
-	
+
 	protected Rectangle bounds;
-	
+
 	protected int x, y, count;
 	protected boolean pickedUp = false;
-	
+
 	public Item(BufferedImage texture, String name, int id) {
 		this.texture = texture;
 		this.name = name;
 		this.id = id;
 		count = 1;
-		
+
 		bounds = new Rectangle(x, y, ITEMWIDTH, ITEMHEIGHT);
-		
+
 		items[id] = this;
 	}
-	
+
 	public void update() {
-		if(handler.getMap().getEntityManager().getPlayer().getCollisionBounds(0f, 0f).intersects(bounds)) {
+		if (handler.getMap().getEntityManager().getPlayer().getCollisionBounds(0f, 0f).intersects(bounds)) {
 			pickedUp = true;
 			handler.getMap().getEntityManager().getPlayer().getInventory().addItem(this);
 		}
 	}
-	
+
 	public void render(Graphics g) {
-		if(handler == null) {
+		if (handler == null) {
 			return;
 		}
-		render(g, (int)(x - handler.getGameCamera().getxOffset()), (int)(y - handler.getGameCamera().getyOffset()));
-		
+		render(g, (int) (x - handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()));
+
 	}
-	
+
 	public void render(Graphics g, int x, int y) {
 		g.drawImage(texture, x, y, ITEMWIDTH, ITEMHEIGHT, null);
 	}
-	
+
 	public Item createNew(int x, int y) {
 		Item i = new Item(texture, name, id);
 		i.setPosition(x, y);
 		return i;
 	}
-	
+
 	public void setPosition(int x, int y) {
 		this.x = x;
 		this.y = y;
 		bounds.x = x;
 		bounds.y = y;
 	}
-	
-	
 
 	public boolean isPickedUp() {
 		return pickedUp;
@@ -132,7 +131,5 @@ public class Item {
 	public int getId() {
 		return id;
 	}
-	
-	
 
 }
--- src/packageRPG/items/ItemManager.java
+++ src/packageRPG/items/ItemManager.java
@@ -7,39 +7,37 @@ import java.util.Iterator;
 import packageRPG.Handler;
 
 public class ItemManager {
-	
+
 	private Handler handler;
 	private ArrayList<Item> items;
-	
+
 	public ItemManager(Handler handler) {
 		this.handler = handler;
 		items = new ArrayList<Item>();
 	}
-	
+
 	public void update() {
 		Iterator<Item> it = items.iterator();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			Item i = it.next();
 			i.update();
-			if(i.isPickedUp()) {
+			if (i.isPickedUp()) {
 				it.remove();
 			}
 		}
 	}
-	
+
 	public void render(Graphics g) {
-		for(Item i : items) {
+		for (Item i : items) {
 			i.render(g);
 		}
-	
-		
+
 	}
-	
+
 	public void addItem(Item i) {
 		i.setHandler(handler);
 		items.add(i);
 	}
-	
 
 	public Handler getHandler() {
 		return handler;
@@ -48,9 +46,7 @@ public class ItemManager {
 	public void setHandler(Handler handler) {
 		this.handler = handler;
 	}
-	
+
 	// getters and setters
-	
-	
 
 }
