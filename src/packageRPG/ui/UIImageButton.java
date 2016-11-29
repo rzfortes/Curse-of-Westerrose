46c2f0e19a1a4d5aded8629a8255695258392e79
Micah Nicole Chu Im <micah_chuim@yahoo.com>
2016-11-29 21:39:21 +0800

    Update UIImageButton.java

--- src/packageRPG/ui/UIImageButton.java
+++ src/packageRPG/ui/UIImageButton.java
@@ -7,6 +7,7 @@ public class UIImageButton extends UIObject {
 
 	private BufferedImage[] images;
 	private ClickListener clicker;
+
 	public UIImageButton(float x, float y, int width, int height, BufferedImage[] images, ClickListener clicker) {
 		super(x, y, width, height);
 		this.images = images;
@@ -15,26 +16,25 @@ public class UIImageButton extends UIObject {
 
 	@Override
 	public void update() {
-		
+
 	}
 
 	@Override
 	public void render(Graphics g) {
-		// makes the button change color so that when the mouse hovers over it, it looks like it
-		if(hovering) {
+		// makes the button change color so that when the mouse hovers over it,
+		// it looks like it
+		if (hovering) {
 			g.drawImage(images[1], (int) x, (int) y, width, height, null);
 		} else {
 			g.drawImage(images[0], (int) x, (int) y, width, height, null);
 		}
-		
+
 	}
 
 	@Override
 	public void onClick() {
 		clicker.onClick();
-		
+
 	}
-	
-	
 
 }
